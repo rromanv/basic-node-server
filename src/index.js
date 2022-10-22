038586472f3581acc259b56ce1627e425255fe99
Rodolfo Roman <rromanv@gmail.com>
2022-10-21 19:27:02 -0500

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+import './utils/config'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import logger from './utils/logger'
+import router from './routes'
+import { notFound, errorHandler } from './utils/errors'
+
+const port = Number(process.env.PORT)
 
 const app = express()
 
-const port = 3000
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(helmet())
 
-app.get('/', (req, res) => {
-  console.log(req)
-  res.send({ msg: 'Hello There' })
-})
+app.use('/', router)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  logger.info(`Server running on port ${port}`)
 })
